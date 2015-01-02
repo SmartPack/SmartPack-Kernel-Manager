341860f70d919190d1a63251a4bcfb966057d85d
Willi Ye <williye97@gmail.com>
2015-01-02 20:49:18 +0100

    Fix navigation drawer on 10 inch tablets

--- app/src/main/java/com/grarak/kerneladiutor/utils/Utils.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Utils.java
@@ -1,11 +1,9 @@
 package com.grarak.kerneladiutor.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -25,21 +23,8 @@ public class Utils implements Constants {
     }
 
     public static boolean isTablet(Context context) {
-        boolean device_large = ((context.getResources().getConfiguration().screenLayout &
-                Configuration.SCREENLAYOUT_SIZE_MASK) ==
-                Configuration.SCREENLAYOUT_SIZE_LARGE);
-
-        if (device_large) {
-            DisplayMetrics metrics = new DisplayMetrics();
-            ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-            if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
-                    || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
-                    || metrics.densityDpi == DisplayMetrics.DENSITY_TV
-                    || metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH)
-                return true;
-        }
-        return false;
+        return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 
     public static int getScreenOrientation(Context context) {
