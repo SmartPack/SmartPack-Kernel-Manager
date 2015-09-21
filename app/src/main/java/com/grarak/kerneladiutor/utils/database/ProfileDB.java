e94c4a16cce292ecc20395e4c5f7345b9b45a902
MrBeetle <bitlo@nn.ru>
2015-09-21 15:37:11 +0600

    Now you cannot create a profile with the same name. If earlier you have profiles with same name, they both saved; Add getProfileId() to ProfileDB; Add confirmation of replacement profile in ProfileFragment;

--- app/src/main/java/com/grarak/kerneladiutor/utils/database/ProfileDB.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/database/ProfileDB.java
@@ -52,9 +52,20 @@ public class ProfileDB extends JsonDB {
         return false;
     }
 
+    public int getProfileId(String name) {
+
+        List<ProfileItem> profiles = getAllProfiles();
+        for (int i = 0; i < profiles.size(); i++) {
+            if (profiles.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public void putProfile(String name, LinkedHashMap<String, String> commands) {
         try {
+
             JSONObject items = new JSONObject();
             items.put("name", name);
 
