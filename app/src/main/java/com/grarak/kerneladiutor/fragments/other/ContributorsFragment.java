1ae14a6d8a61e21899e8b57191c1f3bb84a6c6dc
sunilpaulmathew <sunil.kde@gmail.com>
2017-12-19 15:25:29 +0900

    Contributors: redirecting to my repo
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/grarak/kerneladiutor/fragments/other/ContributorsFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/other/ContributorsFragment.java
@@ -89,7 +89,7 @@ public class ContributorsFragment extends RecyclerViewFragment {
                     error();
                 }
             });
-            mWebpageReader.get("https://api.github.com/repos/Grarak/KernelAdiutor/contributors");
+            mWebpageReader.get("https://api.github.com/repos/SmartPack/SmartPack-Kernel-Manager/contributors");
         }
     }
 
