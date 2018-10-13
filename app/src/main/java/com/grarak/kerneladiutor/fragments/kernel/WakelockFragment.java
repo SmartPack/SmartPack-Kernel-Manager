6f5db077db5f050e454384ddb07e0ed0d4e01379
sunilpaulmathew <sunil.kde@gmail.com>
2018-10-13 09:35:19 +0900

    Wakelocks: re-add a few more wakelocks
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/WakelockFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/WakelockFragment.java
@@ -63,6 +63,7 @@ public class WakelockFragment extends RecyclerViewFragment {
         if (Wakelocks.boefflawlsupported()){
             boefflaWakelockInit(items);
         }
+	wakelockInit(items);
     }
 
     private void boefflaWakelockInit(List<RecyclerViewItem> items){
@@ -145,6 +146,113 @@ public class WakelockFragment extends RecyclerViewFragment {
         String titleA = getString(R.string.wkl_allowed);
         grxbwCardInit(mWakeCard.get(1), titleA, wakelocksinfo, true);
 
+    }
+
+    private void wakelockInit(List<RecyclerViewItem> items) {
+	CardView wakelocks = new CardView(getActivity());
+	wakelocks.setTitle(getString(R.string.wakelocks_other));
+	for (final Wakelocks.Wakelock wakelock : Wakelocks.getWakelocks()) {
+            if (!wakelock.exists()) continue;
+		String description = wakelock.getDescription(getActivity());
+		SwitchView switchView = new SwitchView();
+            if (description == null) {
+		switchView.setSummary(wakelock.getTitle(getActivity()));
+            } else {
+                switchView.setTitle(wakelock.getTitle(getActivity()));
+                switchView.setSummary(description);
+            }
+            switchView.setChecked(wakelock.isEnabled());
+            switchView.addOnSwitchListener(new SwitchView.OnSwitchListener() {
+                @Override
+                public void onChanged(SwitchView switchView, boolean isChecked) {
+                    wakelock.enable(isChecked, getActivity());
+                }
+            });
+             wakelocks.addItem(switchView);
+	}
+	if (Wakelocks.hasWlanrxDivider()) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i < 17; i++) {
+                list.add((100 / i) + "%");
+            }
+            list.add("0%");
+            SeekBarView wlanrxDivider = new SeekBarView();
+            wlanrxDivider.setTitle(getString(R.string.wlan_rx_wakelock_divider));
+            wlanrxDivider.setItems(list);
+            wlanrxDivider.setProgress(Wakelocks.getWlanrxDivider());
+            wlanrxDivider.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    Wakelocks.setWlanrxDivider(position, getActivity());
+                }
+		@Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+            });
+            wakelocks.addItem(wlanrxDivider);
+	}
+	if (Wakelocks.hasWlanctrlDivider()) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i < 17; i++) {
+                list.add((100 / i) + "%");
+            }
+            list.add("0%");
+            SeekBarView wlanctrlDivider = new SeekBarView();
+            wlanctrlDivider.setTitle(getString(R.string.wlan_ctrl_wakelock_divider));
+            wlanctrlDivider.setItems(list);
+            wlanctrlDivider.setProgress(Wakelocks.getWlanctrlDivider());
+            wlanctrlDivider.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    Wakelocks.setWlanctrlDivider(position, getActivity());
+                }
+		@Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+            });
+            wakelocks.addItem(wlanctrlDivider);
+	}
+	if (Wakelocks.hasMsmHsicDivider()) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i < 17; i++) {
+                list.add((100 / i) + "%");
+            }
+            list.add("0%");
+            SeekBarView msmHsicDivider = new SeekBarView();
+            msmHsicDivider.setTitle(getString(R.string.msm_hsic_wakelock_divider));
+            msmHsicDivider.setItems(list);
+            msmHsicDivider.setProgress(Wakelocks.getMsmHsicDivider());
+            msmHsicDivider.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    Wakelocks.setMsmHsicDivider(position, getActivity());
+                }
+		@Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+            });
+            wakelocks.addItem(msmHsicDivider);
+	}
+	if (Wakelocks.hasBCMDHDDivider()) {
+            SeekBarView bcmdhdDivider = new SeekBarView();
+            bcmdhdDivider.setTitle(getString(R.string.bcmdhd_wakelock_divider));
+            bcmdhdDivider.setMax(9);
+            bcmdhdDivider.setMin(1);
+            bcmdhdDivider.setProgress(Wakelocks.getBCMDHDDivider() - 1);
+            bcmdhdDivider.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    Wakelocks.setBCMDHDDivider(position + 1, getActivity());
+                }
+		@Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+            });
+            wakelocks.addItem(bcmdhdDivider);
+	}
+	if (wakelocks.size() > 0) {
+            items.add(wakelocks);
+	}
     }
 
 }
--- app/src/main/java/com/grarak/kerneladiutor/utils/kernel/misc/Wakelocks.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/kernel/misc/Wakelocks.java
@@ -45,6 +45,10 @@ public class Wakelocks {
     private static final String WAKELOCK_BLOCKER = BOEFFLAWL + "/wakelock_blocker";
     private static final String WAKELOCK_BLOCKER_DEFAULT = BOEFFLAWL + "/wakelock_blocker_default";
     private static final String WAKELOCK_SOURCES = "/sys/kernel/debug/wakeup_sources";
+    private static final String WLAN_RX_DIVIDER = "/sys/module/bcmdhd/parameters/wl_divide";
+    private static final String MSM_HSIC_DIVIDER = "/sys/module/xhci_hcd/parameters/wl_divide";
+    private static final String BCMDHD_DIVIDER = "/sys/module/bcmdhd/parameters/wl_divide";
+    private static final String WLAN_CTRL_DIVIDER = "/sys/module/bcmdhd/parameters/wlctrl_divide";
 
     // Wakelocks Order: 0-Name, 1-Time, 2-Wakeups
     private static int mWakelockOrder = 1;
@@ -170,6 +174,148 @@ public class Wakelocks {
 
     public static boolean supported() {
         return boefflawlsupported();
+    }
+
+    public static class Wakelock {
+
+        private final String mPath;
+        @StringRes
+        private final int mTitle;
+        @StringRes
+        private final int mDescription;
+
+        private Boolean mExists;
+
+        private Wakelock(String path) {
+            this(path, 0, 0);
+        }
+
+        private Wakelock(String path, @StringRes int title, @StringRes int description) {
+            mPath = path;
+            mTitle = title;
+            mDescription = description;
+        }
+
+        public String getDescription(Context context) {
+            return mDescription == 0 ? null : context.getString(mDescription);
+        }
+
+        public String getTitle(Context context) {
+            if (mTitle != 0) {
+                return context.getString(mTitle);
+            }
+
+            String[] paths = mPath.split("/");
+            return Utils.upperCaseEachWord(paths[paths.length - 1].replace("enable_", "")
+                    .replace("_ws", "").replace("_", " "));
+        }
+
+        public void enable(boolean enable, Context context) {
+            run(Control.write(enable ? "Y" : "N", mPath), mPath, context);
+        }
+
+        public boolean isEnabled() {
+            return Utils.readFile(mPath).equals("Y");
+        }
+
+        public boolean exists() {
+            if (mExists == null) {
+                return (mExists = Utils.existFile(mPath));
+            }
+            return mExists;
+        }
+
+    }
+
+    private static final List<Wakelock> sWakelocks = new ArrayList<>();
+
+    static {
+        sWakelocks.add(new Wakelock("/sys/module/smb135x_charger/parameters/use_wlock",
+                R.string.smb135x_wakelock, R.string.smb135x_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_smb135x_wake_ws",
+                R.string.smb135x_wakelock, R.string.smb135x_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_si_ws",
+                R.string.sensor_ind_wakelock, R.string.sensor_ind_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_msm_hsic_ws",
+                R.string.msm_hsic_host_wakelock, R.string.msm_hsic_host_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/wlan_rx_wake",
+                R.string.wlan_rx_wakelock, R.string.wlan_rx_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_wlan_rx_wake_ws",
+                R.string.wlan_rx_wakelock, R.string.wlan_rx_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/wlan_ctrl_wake",
+                R.string.wlan_ctrl_wakelock, R.string.wlan_ctrl_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_wlan_ctrl_wake_ws",
+                R.string.wlan_ctrl_wakelock, R.string.wlan_ctrl_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/wlan_wake",
+                R.string.wlan_wakelock, R.string.wlan_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_wlan_wake_ws",
+                R.string.wlan_wakelock, R.string.wlan_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_bluesleep_ws",
+                R.string.bluesleep_wakelock, R.string.bluesleep_wakelock_summary));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_ipa_ws"));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_netlink_ws"));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_qcom_rx_wakelock_ws"));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_timerfd_ws"));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_wlan_extscan_wl_ws"));
+        sWakelocks.add(new Wakelock("/sys/module/wakeup/parameters/enable_wlan_ws"));
+    }
+
+    public static void setBCMDHDDivider(int value, Context context) {
+        run(Control.write(String.valueOf(value), BCMDHD_DIVIDER), BCMDHD_DIVIDER, context);
+    }
+
+    public static int getBCMDHDDivider() {
+        return Utils.strToInt(Utils.readFile(BCMDHD_DIVIDER));
+    }
+
+    public static boolean hasBCMDHDDivider() {
+        return Utils.existFile(BCMDHD_DIVIDER);
+    }
+
+    public static void setMsmHsicDivider(int value, Context context) {
+        run(Control.write(String.valueOf(value == 15 ? 0 : value + 1), MSM_HSIC_DIVIDER),
+                MSM_HSIC_DIVIDER, context);
+    }
+
+    public static int getMsmHsicDivider() {
+        int value = Utils.strToInt(Utils.readFile(MSM_HSIC_DIVIDER));
+        return value == 0 ? 16 : value - 1;
+    }
+
+    public static boolean hasMsmHsicDivider() {
+        return Utils.existFile(MSM_HSIC_DIVIDER);
+    }
+
+    public static void setWlanrxDivider(int value, Context context) {
+        run(Control.write(String.valueOf(value == 15 ? 0 : value + 1), WLAN_RX_DIVIDER),
+                WLAN_RX_DIVIDER, context);
+    }
+
+    public static int getWlanrxDivider() {
+        int value = Utils.strToInt(Utils.readFile(WLAN_RX_DIVIDER));
+        return value == 0 ? 16 : value - 1;
+    }
+
+    public static boolean hasWlanrxDivider() {
+        return Utils.existFile(WLAN_RX_DIVIDER);
+    }
+
+    public static void setWlanctrlDivider(int value, Context context) {
+        run(Control.write(String.valueOf(value == 15 ? 0 : value + 1), WLAN_CTRL_DIVIDER),
+                WLAN_CTRL_DIVIDER, context);
+    }
+
+    public static int getWlanctrlDivider() {
+        int value = Utils.strToInt(Utils.readFile(WLAN_CTRL_DIVIDER));
+        return value == 0 ? 16 : value - 1;
+    }
+
+    public static boolean hasWlanctrlDivider() {
+        return Utils.existFile(WLAN_CTRL_DIVIDER);
+    }
+
+    public static List<Wakelock> getWakelocks() {
+        return sWakelocks;
     }
 
     private static void run(String command, String id, Context context) {
